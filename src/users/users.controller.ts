82d2e3c4d6ba346d514996cf1206c04ae881baa8
Or Hasson <hassonor@gmail.com>
2024-07-27 23:59:45 +0300

    add patch dto,controller and endpoint, use PartialType showcase

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,38 @@
 import {
-  Body,
   Controller,
   Get,
   Param,
   Post,
+  Patch,
   Query,
-  Headers,
-  Ip,
   ParseIntPipe,
   DefaultValuePipe,
-  ValidationPipe,
+  Body,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
+import { PatchUserDto } from './dtos/patch-user.dto';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id?')
   public getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
+    @Param() getUserParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log(limit);
-    console.log(page);
+    console.log(getUserParamDto);
     return 'You sent a get request to users endpoint';
   }
 
   @Post()
-  public createUsers(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
-    console.log(createUserDto);
+  public createUsers(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto instanceof CreateUserDto);
     return 'You sent a post request to users endpoint';
+  }
+
+  @Patch()
+  public patchUser(@Body() patchUserDto: PatchUserDto) {
+    return 'You sent a patch request to users endpoint';
   }
 }
