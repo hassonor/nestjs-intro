fa6a939f2e4487e5130b712184247b8e36300295
Or Hasson <hassonor@gmail.com>
2024-07-30 00:43:51 +0300

    add patch post dto,method and endpoint

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,20 +1,41 @@
-import { Body, Controller, Get, Post, Param } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Patch } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
 import { CreatePostDto } from './dtos/create-post.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { PatchPostDto } from './dtos/patch-post-dto';
 
 @Controller('posts')
 @ApiTags('Posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @Get('/:userId')
+  public getPosts(@Param('userId') userId: string) {
+    return this.postsService.findAll(userId);
+  }
+
+  @ApiOperation({
+    summary: 'Create a new blog post',
+  })
+  @ApiResponse({
+    status: 201,
+    description:
+      'You get a 201 status code if your post is created successfully. ',
+  })
   @Post()
   public createPost(@Body() createPostDto: CreatePostDto) {
     return this.postsService.createPost(createPostDto);
   }
 
-  @Get('/:userId')
-  public getPosts(@Param('userId') userId: string) {
-    return this.postsService.findAll(userId);
+  @ApiOperation({
+    summary: 'Update an existing blog post',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'A 200 status code if your post is updated successfully. ',
+  })
+  @Patch()
+  public updatePost(@Body() patchPostDto: PatchPostDto) {
+    console.log(patchPostDto);
   }
 }
