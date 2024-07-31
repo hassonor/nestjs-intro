def929b284fb2178638ebbfc8273937a35503817
Or Hasson <hassonor@gmail.com>
2024-07-31 22:06:44 +0300

    setup compadoc package

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -2,12 +2,27 @@ import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 import { AuthService } from '../../auth/providers/auth.service';
 
+/**
+ * Service to handle business logic related to users.
+ */
 @Injectable()
 export class UserService {
+  /**
+   * Constructs the UserService and injects the AuthService.
+   * @param {AuthService} authService - The authentication service.
+   */
   constructor(
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
+
+  /**
+   * Retrieves all users based on the provided parameters.
+   * @param {GetUsersParamDto} getUserParamDto - The parameters for getting users.
+   * @param {number} limit - The number of users to retrieve.
+   * @param {number} page - The page number for pagination.
+   * @returns {Array<{firstName: string, email: string}>} - A list of users.
+   */
   public findAll(
     getUserParamDto: GetUsersParamDto,
     limit: number,
@@ -27,6 +42,11 @@ export class UserService {
     ];
   }
 
+  /**
+   * Finds a single user by their ID.
+   * @param {string} id - The ID of the user.
+   * @returns {{id: number, firstName: string, email: string}} - The user details.
+   */
   public findOneById(id: string) {
     return {
       id: 1234,
