a53078bb5510245d825e823d5aacdbee046fbad2
Or Hasson <hassonor@gmail.com>
2024-08-05 14:16:49 +0300

    add create post with oneToOne relation

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -4,10 +4,11 @@ import { PostsController } from './posts.controller';
 import { PostsService } from './providers/posts.service';
 import { UsersModule } from '../users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MetaOption } from '../meta-options/meta-option.entity';
 
 @Module({
   controllers: [PostsController],
   providers: [PostsService],
-  imports: [UsersModule, TypeOrmModule.forFeature([Post])],
+  imports: [UsersModule, TypeOrmModule.forFeature([Post, MetaOption])],
 })
 export class PostsModule {}
--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,12 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
 import { CreatePostDto } from '../dtos/create-post.dto';
 import { UserService } from '../../users/providers/users.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MetaOption } from '../../meta-options/meta-option.entity';
+import { Repository } from 'typeorm';
+import { Post } from '../post.entity';
 
 @Injectable()
 export class PostsService {
-  constructor(private readonly usersServer: UserService) {}
-  public createPost(createPostDto: CreatePostDto) {
-    return 'You sent a post request to posts endpoint';
+  constructor(
+    private readonly usersServer: UserService,
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+    @InjectRepository(MetaOption)
+    private readonly metaOptionsRepository: Repository<MetaOption>,
+  ) {}
+  public async createPost(@Body() createPostDto: CreatePostDto) {
+    // Create metaOptions
+    const metaOption = createPostDto.metaOptions
+      ? this.metaOptionsRepository.create(createPostDto.metaOptions)
+      : null;
+    if (metaOption) {
+      await this.metaOptionsRepository.save(metaOption);
+    }
+    const post = this.postRepository.create(createPostDto);
+
+    if (metaOption) {
+      post.metaOptions = metaOption;
+    }
+
+    return await this.postRepository.save(post);
   }
 
   public findAll(userId: string) {
